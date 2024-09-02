e52bd7c9c90d800c1c0b6c4ffcb31e4e1a08b98d
Farakh Ali <farakh.ali@rakurai.io>
2024-09-02 13:04:58 +0500

    handle script crash in case of slot skipped

--- block_metrics_analyzer.py
+++ block_metrics_analyzer.py
@@ -15,7 +15,6 @@ db_base_url = (
 )
 
 
-
 class RateLimiter:
     def __init__(self, max_requests_per_second, logger=None):
         self.max_requests_per_second = max_requests_per_second
@@ -127,58 +126,67 @@ def get_current_slot(url):
     return response
 
 
-def get_block_for_slot(rpc_url, slot, rpc_client:Client, db, leader_identity):
+def get_block_for_slot(rpc_url, slot, rpc_client: Client, db, leader_identity):
     total_txn = 0
     total_vote_txn = 0
     total_cu = 0
     block_reward = 0
-    
-    max_retries=3
+    leader_bank_time_ms = 0
+    replay_time_ms = 0
+
+    max_retries = 3
     attempt = 0
     while attempt < max_retries:
         try:
             logger.info(f"Calculating block reward for slot {slot}")
             rate_limiter.check_rate_limit()
-            resp = rpc_client.get_block(
-                slot, max_supported_transaction_version=0
-            )
-            total_txn =resp.value.transactions.__len__()
+            resp = rpc_client.get_block(slot, max_supported_transaction_version=0)
+
+            if resp is None or resp.value is None:
+                logger.info(f"Slot {slot} was skipped or missing. Returning None.")
+                break
+
+            total_txn = len(resp.value.transactions)
             block_reward += resp.value.rewards[0].lamports
+
             for each_txn in resp.value.transactions:
                 total_cu += each_txn.meta.compute_units_consumed
-                for eac_acc in  each_txn.transaction.message.account_keys:
-                    if eac_acc.__str__() == "Vote111111111111111111111111111111111111111":
-                        total_vote_txn +=1
-            leader_bank_time_ms = fetch_leader_bank_time(slot, rpc_url, db, leader_identity)
+                for each_acc in each_txn.transaction.message.account_keys:
+                    if str(each_acc) == "Vote111111111111111111111111111111111111111":
+                        total_vote_txn += 1
+
+            leader_bank_time_ms = fetch_leader_bank_time(
+                slot, rpc_url, db, leader_identity
+            )
             replay_time_ms = fetch_next_leader_replay_time(slot, rpc_url, db)
-            return {
-                    "slot": slot,
-                    "total_txn": total_txn,
-                    "total_vote_txn": total_vote_txn,
-                    "other_txns": total_txn - total_vote_txn,
-                    "total_cu": total_cu,
-                    "cu_x": round(total_cu / 48000000, 2),
-                    "leader_time_ms": round(leader_bank_time_ms, 2),
-                    "replay_time_ms": round(replay_time_ms, 2),
-                    "block_rewards": round(block_reward, 2),
-                    "block_rewards_sol": round(block_reward/1000000000, 3)
-            }
             break
         except Exception as e:
             attempt += 1
-            logger.error(
-                f"Error in RPC for slot {slot}, retry attempt {attempt} {e}"
-            )
- 
+            logger.error(f"Error in RPC for slot {slot}, retry attempt {attempt}: {e}")
+
+    return {
+        "slot": slot,
+        "total_txn": total_txn,
+        "total_vote_txn": total_vote_txn,
+        "other_txns": total_txn - total_vote_txn,
+        "total_cu": total_cu,
+        "cu_x": round(total_cu / 48000000, 2),
+        "leader_time_ms": round(leader_bank_time_ms, 2),
+        "replay_time_ms": round(replay_time_ms, 2),
+        "block_rewards": round(block_reward, 2),
+        "block_rewards_sol": round(block_reward / 1000000000, 3),
+    }
+
+
 def process_slots(args, db):
     try:
         rpc_client = connect_rpc_client(args.rpc_url, rate_limiter)
         slots = [args.start_slot + i for i in range(args.count)]
         logger.info(f"{rpc_client.get_slot().value}")
         # leader_identity = rpc_client.get_slot_leaders(int(args.start_slot)) # method not supported in lib(solana) need to query using post
-        leader_identity= get_slot_leader(int(args.start_slot), args.rpc_url)
+        leader_identity = get_slot_leader(int(args.start_slot), args.rpc_url)
         logger.info(f"Leader identity at slot {args.start_slot} is {leader_identity}")
-        with open(f"{args.start_slot}-metrics.csv", mode="w", newline="") as file:
+        with open(f"metrics.csv", mode="w", newline="") as file:
             writer = csv.DictWriter(
                 file,
                 fieldnames=[
@@ -197,12 +205,15 @@ def process_slots(args, db):
             writer.writeheader()
             for each_slot in slots:
                 logger.info(f"processing slot {each_slot}")
-                result = get_block_for_slot(args.rpc_url, each_slot, rpc_client, db, leader_identity)
+                result = get_block_for_slot(
+                    args.rpc_url, each_slot, rpc_client, db, leader_identity
+                )
                 writer.writerow(result)
                 time.sleep(2)
     except Exception as e:
         msg = f"Error: error block metrics calculation {e}"
         logger.info(msg)
+
 
 def main():
     parser = argparse.ArgumentParser(
@@ -225,7 +236,10 @@ def main():
         "--start_slot", type=int, help="The starting slot to process.", required=True
     )
     parser.add_argument(
-        "--count", type=int, default=4, help="Number of slots to process starting from  `--start-slot`, default is 4."
+        "--count",
+        type=int,
+        default=5,
+        help="Number of slots to process starting from  `--start-slot`, default is 4.",
     )
     parser.add_argument(
         "--req_per_sec",
@@ -237,11 +251,10 @@ def main():
 
 
 if __name__ == "__main__":
-    
-    args = main()
-    
 
-     # Logging Setup
+    args = main()
+
+    # Logging Setup
     logger = logging.getLogger()
     logger.setLevel(logging.INFO)
     log_file_name = f"{time.strftime('%Y-%m-%d%H-%M-%S')}.log"
@@ -252,18 +265,17 @@ if __name__ == "__main__":
     handler.setLevel(logging.DEBUG)
     logger.addHandler(handler)
     rate_limiter = RateLimiter(int(args.req_per_sec), logger=logger)
-    
+
     logger.info(
         f"Getting (cluster = {args.cluster}) data about block from {args.start_slot} to {args.start_slot + args.count - 1}"
     )
 
     logger.info(f"Cluster: {args.cluster}")
     logger.info(f"RPC URL: {args.rpc_url}")
-    
+
     if args.cluster == "m":
-        process_slots(args,"mainnet-beta")
+        process_slots(args, "mainnet-beta")
     elif args.cluster == "t":
         process_slots(args, "tds")
     else:
         logger.info("invalid cluster type {use m=mainnet, t=testnet}")
-
